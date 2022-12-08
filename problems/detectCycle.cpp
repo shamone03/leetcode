c727de8be2292966dcde40437e0d9d82076e376e
Aryah <ar341476@dal.ca>
2022-12-08 17:53:08 -0400

    detecting cycle

---
+++ problems/detectCycle.cpp
@@ -0,0 +1,15 @@
+#include "../datastructs/ListNode.h"
+#include <map>
+
+ListNode* detectCycle(ListNode* head) {
+    std::map<ListNode*, int> visited;
+    int index = 0;
+    for (ListNode* node = head; node != nullptr; node = node->next) {
+        if (visited.count(node)) {
+            return node;
+        }
+        visited.insert(std::pair<ListNode*, int>(node, index));
+    }
+    return nullptr;
+    
+}
