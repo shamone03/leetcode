200f9826994052ef90ae685e428f624a6e3898a4
Aryah <ar341476@dal.ca>
2022-12-07 21:35:48 -0400

    isisomorphic

---
+++ problems/isIsomorphic.cpp
@@ -0,0 +1,22 @@
+#include <string>
+
+bool isIsomorphic(std::string s, std::string t) {
+    
+    int x[255] = {};
+    int y[255] = {};
+    for (int i = 0; i < s.length(); i++) {
+        if (x[s[i]] == 0) {
+            x[s[i]] = t[i];
+        }
+        if (y[t[i]] == 0) {
+            y[t[i]] = s[i];
+        }
+        if (x[s[i]] != t[i]) {
+            return false;
+        }
+        if (y[t[i]] != s[i]) {
+            return false;
+        }
+    }
+    return true;
+}
