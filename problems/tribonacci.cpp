a3b6e9ac18acbd64d4fc164c12fe7a134770935f
Aryah <ar341476@dal.ca>
2023-01-30 15:19:50 -0400

    tribonacci dynamic

--- problems/tribonacci.cpp
+++ problems/tribonacci.cpp
@@ -1,8 +1,21 @@
-int tribonacci(int n) {
+#include <iostream>
+
+int helper(int n, int* memo) {
+    if (memo[n] != -1) {
+        return memo[n];
+    }
     if (n == 0) return 0;
     if (n == 1) return 1;
     if (n == 2) return 1;
 
-    return tribonacci(n - 3) + tribonacci(n - 2) + tribonacci(n - 1);
-     
+    memo[n - 3] = helper(n - 3, memo);
+    memo[n - 2] = helper(n - 2, memo);
+    memo[n - 1] = helper(n - 1, memo);
+    return memo[n - 3] + memo[n - 2] + memo[n - 1];
+}
+
+int tribonacci(int n) {
+    int* memo = new int[n + 1];
+    for (int i = 0; i < (n + 1); memo[i++] = -1);
+    return helper(n, memo);
 }
