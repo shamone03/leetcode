f011e851030f63b8d0f7a2650a4202967dcd17b2
Aryah <ar341476@dal.ca>
2023-05-05 20:57:10 -0300

    renamed

--- problems/searchMatrix.cpp
+++ problems/searchMatrix.cpp
@@ -7,13 +7,13 @@ using std::vector;
 bool searchMatrix(vector<vector<int>>& matrix, int target) {
     int s = matrix[0].size();
     int l = 0;
-    int rnorm = ((matrix.size() - 1) * s) + matrix[0].size() - 1;
+    int r = ((matrix.size() - 1) * s) + matrix[0].size() - 1;
 
-    int mid = (rnorm - l) / 2;
+    int mid = (r - l) / 2;
     int m[2]{0};
 
-    while (l <= rnorm) {
-        mid = l + ((rnorm - l) / 2);
+    while (l <= r) {
+        mid = l + ((r - l) / 2);
         m[0] = mid / s;
         m[1] = mid % s;
         if (matrix[m[0]][m[1]] == target) {
@@ -23,7 +23,7 @@ bool searchMatrix(vector<vector<int>>& matrix, int target) {
             l = mid + 1;
         }
         if (matrix[m[0]][m[1]] > target) {
-            rnorm = mid - 1;
+            r = mid - 1;
         }
     }
 
