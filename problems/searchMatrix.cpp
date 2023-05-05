6a24bb3c02dbdebb264d5949041d589d65add0d2
Aryah <ar341476@dal.ca>
2023-05-05 20:47:54 -0300

    simplifed

--- problems/searchMatrix.cpp
+++ problems/searchMatrix.cpp
@@ -6,24 +6,24 @@ using std::vector;
 
 bool searchMatrix(vector<vector<int>>& matrix, int target) {
     int s = matrix[0].size();
-    int lnorm = 0;
+    int l = 0;
     int rnorm = ((matrix.size() - 1) * s) + matrix[0].size() - 1;
 
-    int mnorm = (rnorm - lnorm) / 2;
+    int mid = (rnorm - l) / 2;
     int* m = new int[2]{};
 
-    while (lnorm <= rnorm) {
-        mnorm = lnorm + ((rnorm - lnorm) / 2);
-        m[0] = mnorm / s;
-        m[1] = mnorm % s;
+    while (l <= rnorm) {
+        mid = l + ((rnorm - l) / 2);
+        m[0] = mid / s;
+        m[1] = mid % s;
         if (matrix[m[0]][m[1]] == target) {
             return true;
         }
         if (matrix[m[0]][m[1]] < target) {
-            lnorm = mnorm + 1;
+            l = mid + 1;
         }
         if (matrix[m[0]][m[1]] > target) {
-            rnorm = mnorm - 1;
+            rnorm = mid - 1;
         }
     }
 
