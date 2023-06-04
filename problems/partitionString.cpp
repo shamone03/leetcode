2c0eb386d7889e17747cd44106fb544d561f0606
Aryah <ar341476@dal.ca>
2023-06-04 18:05:44 -0300

    partition string

---
+++ problems/partitionString.cpp
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/optimal-partition-of-string/?envType=study-plan-v2&envId=amazon-spring-23-high-frequency
+#include <string>
+#include <unordered_set>
+
+using std::string, std::unordered_set;
+
+int partitionString(string s) {
+    int partitions = 0;
+    unordered_set<char> set;
+    for (int i = 0; i < s.size(); i++) {
+        if (!set.insert(s[i]).second) {
+            partitions++;
+            i--;
+            set.clear();
+        }
+    }
+
+    return partitions;
+}
